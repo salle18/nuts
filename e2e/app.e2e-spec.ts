a80aac0a34b08f885b6c505d7c733f2f0200c064
Angular CLI <angular-cli@angular.io>
2017-07-25 19:52:35 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { NutsPage } from './app.po';
+
+describe('nuts App', () => {
+  let page: NutsPage;
+
+  beforeEach(() => {
+    page = new NutsPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class NutsPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
